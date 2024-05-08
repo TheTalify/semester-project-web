a26cd75b160ae5fd49e68b9a250c3aba4f632a62
Muhammad Talha <talhamushtaq73@gmail.com>
2024-05-08 20:09:23 +0500

    Warning Fixes

--- src/components/Homepage/Navbar.js
+++ src/components/Homepage/Navbar.js
@@ -6,7 +6,7 @@ class Navbar extends React.Component {
     return (
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
         <div className="container-fluid">
-          {/* <a href="index.html" style={{ paddingRight: "10px" }}>
+          {/*<a href="index.html" style={{ paddingRight: "10px" }}>
             <svg
               id="logo-89"
               width="30"

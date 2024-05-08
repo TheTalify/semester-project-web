01c778a2970995056326eece3e829d8eaa5edd48
Aamna Khan <aamnakhan512@gmail.com>
2024-05-08 15:51:43 +0500

    Window

---
+++ src/Logo.js
@@ -0,0 +1,12 @@
+export default function Logo() {
+  return (
+    
+      <img
+        src="../images/shopping.png"
+        alt="cart"
+        width="28"
+        height="28"
+        padding-right={10}
+      />
+  );
+}

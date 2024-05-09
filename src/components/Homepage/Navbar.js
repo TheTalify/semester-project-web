07d5a5e2926b57c994e5c1fef068427b8089ebe3
Aamna Khan <aamnakhan512@gmail.com>
2024-05-09 13:35:54 +0500

    navbar updated

--- src/components/Homepage/Navbar.js
+++ src/components/Homepage/Navbar.js
@@ -1,12 +1,14 @@
 import React from "react";
+import "./navbar.css";
 import Logo from "../../Logo.js";
+
 class Navbar extends React.Component {
   state = {};
   render() {
     return (
-      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <nav className="navbar navbar-expand-lg navbar-dark bg-mint">
         <div className="container-fluid">
-          {/*<a href="index.html" style={{ paddingRight: "10px" }}>
+          {/* <a href="index.html" style={{ paddingRight: "10px" }}>
             <svg
               id="logo-89"
               width="30"
@@ -22,7 +24,7 @@ class Navbar extends React.Component {
               ></path>
               <path
                 class="ccustom"
-                d="M18.5003 39.9444V32. 7998C18.5003 26.559 13.4411 21.4998 7.20026 21.4998H0.0556641C0.785998 31.3497 8.65036 39.214 18.5003 39.9444Z"
+                d="M18.5003 39.9444V32.7998C18.5003 26.559 13.4411 21.4998 7.20026 21.4998H0.0556641C0.785998 31.3497 8.65036 39.214 18.5003 39.9444Z"
                 fill="#775732"
               ></path>
               <path
@@ -30,14 +32,14 @@ class Navbar extends React.Component {
                 d="M39.9449 18.4998C39.2145 8.64987 31.3502 0.78551 21.5003 0.0551758V7.19977C21.5003 13.4406 26.5594 18.4998 32.8003 18.4998H39.9449Z"
                 fill="#775732"
               ></path>
-              <path 
+              <path
                 class="ccustom"
                 d="M18.5003 0.0551758C8.65036 0.78551 0.785998 8.64987 0.0556641 18.4998H7.20026C13.4411 18.4998 18.5003 13.4406 18.5003 7.19977V0.0551758Z"
                 fill="#775732"
               ></path>
               <path
                 class="ccustom"
-                d="M13.583 19.9998C16.3555 18.6145 18.615 16.335  20.0002 13.5825C21.3855 16.355 23.6449 18.6145 26.4175 19.9998C23.6449 21.385 21.3855 23.6445 20.0002 26.417C18.615 23.6445 16.3555 21.385 13.583 19.9998Z"
+                d="M13.583 19.9998C16.3555 18.6145 18.615 16.355 20.0002 13.5825C21.3855 16.355 23.6449 18.6145 26.4175 19.9998C23.6449 21.385 21.3855 23.6445 20.0002 26.417C18.615 23.6445 16.3555 21.385 13.583 19.9998Z"
                 fill="#CA9352"
               ></path>
             </svg>
@@ -47,7 +49,7 @@ class Navbar extends React.Component {
             <Logo />
             <span style={{ paddingLeft: "5px" }}>PakZone</span>
           </a>
-          
+
           <button
             className="navbar-toggler"
             type="button"
@@ -120,11 +122,11 @@ class Navbar extends React.Component {
               </button>
             </form> */}
             <div className="navbar-buttons">
-              <button className="btn" onClick={handleLogin} style={{ backgroundImage: `url(./images/user-btn.png)` }}>
-                Login
+              <button className="btn profile-btn" onClick={handleLogin}>
+                <Profile />
               </button>
-              <button className="btn" onClick={handleCart} style={{ backgroundImage: `url(cart.png)` }}>
-                Cart
+              <button className="btn cart-btn" onClick={handleCart}>
+                <Cart />
               </button>
             </div>
           </div>
@@ -141,5 +143,16 @@ function handleLogin() {
 function handleCart() {
   // Handle cart button click
 }
-
+function Cart() {
+  return (
+      <img src="../images/cart-btn.png" width={20} height={20} alt="cart" />
+ 
+  );
+}
+function Profile() {
+  return (
+    <img src="../images/user-btn.png" width={20} height={20} alt="profile" />
+  
+  );
+}
 export default Navbar;

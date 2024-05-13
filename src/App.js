c3cc97155c0a83bce47989fc75d9add91d50b188
Muhammad Talha <talhamushtaq73@gmail.com>
2024-05-13 14:41:24 +0500

    Added a Login and Signup page with backed Functionality

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
-import Navbar from './components/Homepage/Navbar';
+import Navbar from './components/Mainpage/Navbar/navbar';
+import Main from './components/Mainpage'
+import {Route, Routes, Navigate} from 'react-router-dom'
+import Signup from './components/Signup'
+import Login from './components/Login'
 function App() {
+  const user = localStorage.getItem('token')
   return (
-    <>
-    <Navbar/>
-    </>
+    <Routes>
+			{user && <Route path="/" exact element={<Navbar />} />}
+			<Route path="/signup" exact element={<Signup />} />
+			<Route path="/login" exact element={<Login />} />
+			<Route path="/" element={<Navigate replace to="/login" />} />
+		</Routes>
   );
 }
 
---
+++ src/components/Mainpage/index.jsx
@@ -0,0 +1,58 @@
+import React, { useState, useEffect } from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+import LoginPage from "../Login/index";
+import Navbar from "./Navbar/navbar";
+import MainContent from "./MainContent";
+
+const Main = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    // Check if the user is logged in by verifying the presence of a session token
+    const token = localStorage.getItem("token");
+    setIsLoggedIn(!!token); // Update the isLoggedIn state based on token presence
+  }, []);
+
+  const handleLogin = (token) => {
+    // Handle login action
+    localStorage.setItem("token", token);
+    setIsLoggedIn(true); // Update the isLoggedIn state to true
+  };
+
+  const handleLogout = () => {
+    // Handle logout action
+    localStorage.removeItem("token");
+    setIsLoggedIn(false); // Update the isLoggedIn state to false
+  };
+
+  return (
+    <>
+      <Navbar isLoggedIn={isLoggedIn} onLogout={handleLogout} />
+      <Routes>
+        <Route path="/" element={<MainContent />} />
+        <Route
+          path="/login"
+          element={
+            isLoggedIn ? (
+              <Navigate to="/" />
+            ) : (
+              <LoginPage onLogin={handleLogin} />
+            )
+          }
+        />
+        <Route
+          path="/:userID"
+          element={
+            isLoggedIn ? (
+              <MainContent />
+            ) : (
+              <Navigate to="/login" />
+            )
+          }
+        />
+      </Routes>
+    </>
+  );
+};
+
+export default Main;

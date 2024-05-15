5cecfb085f32939251d5012dae683484c752c3a0
Aamna Khan <aamnakhan512@gmail.com>
2024-05-15 12:19:52 +0500

    Homepage created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
-import Navbar from './components/Homepage/Navbar';
+import Artisan from './Pages/Artisan';
+import Cart from './Pages/Cart';
+import Home from './components/Mainpage/Home';
+import KuchOr from './Pages/KuchOr';
+import LoginSignup from './Pages/LoginSignup';
+import Shop from './Pages/Shop';
+import Navbar from './components/Mainpage/Navbar/Navbar';
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
 function App() {
   return (
-    <>
-    <Navbar/>
-    </>
+    <div>
+      <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/shop' element={<Shop/>}/>
+        <Route path='/kuchOr' element={<KuchOr/>}/>
+        <Route path='/artisans' element={<Artisan/>}/>
+        <Route path='/cart' element={<Cart/>}/>
+        <Route path='/loginSignup' element={<LoginSignup/>}/>
+      </Routes>
+
+
+      </BrowserRouter>
+    </div>
   );
 }
 

657b6b59cd4290a5634fbb2c6e4849a6e8fac747
Muhammad Talha <talhamushtaq73@gmail.com>
2024-05-15 19:20:33 +0500

    Login Page fixed and seperated. Bugged login Still Needs reloading everytime

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import './App.css';
-import Navbar from './components/Mainpage/Navbar/Navbar';
-import Main from './components/Mainpage'
-import {Route, Routes, Navigate} from 'react-router-dom'
-import Signup from './components/Signup'
-import Login from './components/Login'
+import { Routes, Route, Navigate } from 'react-router-dom';
+import Main from './components/Mainpage';
+import Signup from './components/Signup';
+import Login from './components/Login';
+
 function App() {
-  const user = localStorage.getItem('token')
+  const user = localStorage.getItem('token');
+
   return (
     <Routes>
-			{user && <Route path="/" exact element={<Navbar />} />}
-			<Route path="/signup" exact element={<Signup />} />
-			<Route path="/login" exact element={<Login />} />
-			<Route path="/" element={<Navigate replace to="/login" />} />
-		</Routes>
+      <Route path="/*" element={user ? <Main /> : <Navigate replace to="/login" />} />
+      <Route path="/signup" element={user ? <Navigate replace to="/" /> : <Signup />} />
+      <Route path="/login" element={user ? <Navigate replace to="/" /> : <Login />} />
+    </Routes>
   );
 }
 
--- src/components/Mainpage/index.jsx
+++ src/components/Mainpage/index.jsx
@@ -1,60 +1,48 @@
 import React, { useState, useEffect } from "react";
-import { Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route, Navigate, useNavigate } from "react-router-dom";
 import LoginPage from "../Login/index";
 import Navbar from "./Navbar/Navbar";
 import MainContent from "./MainContent";
-import Hero from './Hero/Hero'
-import Popular from './Popular/Popular'
+import Hero from './Hero/Hero';
+import Popular from './Popular/Popular';
 
 const Main = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const navigate = useNavigate();
 
   useEffect(() => {
-    // Check if the user is logged in by verifying the presence of a session token
     const token = localStorage.getItem("token");
-    setIsLoggedIn(!!token); // Update the isLoggedIn state based on token presence
+    setIsLoggedIn(!!token);
   }, []);
 
   const handleLogin = (token) => {
-    // Handle login action
     localStorage.setItem("token", token);
-    setIsLoggedIn(true); // Update the isLoggedIn state to true
+    setIsLoggedIn(true);
+    window.location.reload(); // Navigate to the homepage after login
   };
 
   const handleLogout = () => {
-    // Handle logout action
     localStorage.removeItem("token");
-    setIsLoggedIn(false); // Update the isLoggedIn state to false
+    setIsLoggedIn(false);
+    window.location.reload(); // Navigate to the login page after logout
   };
 
   return (
     <>
-      <div>
       <Navbar isLoggedIn={isLoggedIn} onLogout={handleLogout} />
-      <Hero/>
-      <Popular/>
-    </div>
       <Routes>
-        <Route path="/" element={<MainContent />} />
+        <Route path="/" element={<>
+            <Hero />
+            <Popular />
+            {/* <MainContent /> */}
+        </>} />
         <Route
           path="/login"
-          element={
-            isLoggedIn ? (
-              <Navigate to="/" />
-            ) : (
-              <LoginPage onLogin={handleLogin} />
-            )
-          }
+          element={isLoggedIn ? <Navigate to="/" /> : <LoginPage onLogin={handleLogin} />}
         />
         <Route
           path="/:userID"
-          element={
-            isLoggedIn ? (
-              <MainContent />
-            ) : (
-              <Navigate to="/login" />
-            )
-          }
+          element={isLoggedIn ? <MainContent /> : <Navigate to="/login" />}
         />
       </Routes>
     </>
